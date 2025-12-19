733ce67e559027a1f567c2acd449856ee6170377
k-tajika <k-tajika@jaist.ac.jp>
2025-12-19 10:13:52 +0900

    check add

--- common/scripts/11.deploy-cAdvisor/3.check-after-deploy.sh
+++ common/scripts/11.deploy-cAdvisor/3.check-after-deploy.sh
@@ -8,3 +8,8 @@ kubectl logs -n kube-system -l name=cadvisor --tail=50
 
 # ServiceMonitorの確認
 kubectl get servicemonitor cadvisor -n kube-system
+
+# Prometheus にポートフォワード
+echo "kubectl port-forward -n monitoring svc/prometheus-kube-prometheus-prometheus 9090:9090"
+echo " ブラウザで http://localhost:9090 にアクセス"
+echo " クエリ: kepler_node_package_joules_total"
