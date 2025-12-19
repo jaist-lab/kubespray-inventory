3c3985b6e887f50d0f04482308af0eed4dae3dc4
k-tajika <k-tajika@jaist.ac.jp>
2025-12-19 10:06:53 +0900

    デプロイスクリプトを修正

--- common/scripts/11.deploy-cAdvisor/2.deploy.sh
+++ common/scripts/11.deploy-cAdvisor/2.deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# マニフェストファイルを適用
-kubectl apply -f cadvisor-daemonset.yaml
+# 既存のリソースをクリーンアップ
+kubectl delete service cadvisor -n kube-system --ignore-not-found
+kubectl delete servicemonitor cadvisor -n monitoring --ignore-not-found
+kubectl delete daemonset cadvisor -n kube-system --ignore-not-found
 
-# デプロイ状況確認
+# ServiceAccountを作成
+kubectl create serviceaccount cadvisor -n kube-system
+
+# 修正済みYAMLを適用
+kubectl apply -f cadvisor-daemonset-fixed.yaml
+
+# デプロイ状況を確認
 kubectl get daemonset cadvisor -n kube-system
-
-# 全ノードでの稼働確認
 kubectl get pods -n kube-system -l name=cadvisor -o wide
+
+# ServiceMonitorの確認
+kubectl get servicemonitor -n monitoring cadvisor
