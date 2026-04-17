199ea3251539e37df63fd183bf2c8c61bbba1f23
jaistlab <jaistlab@jaist.ac.jp>
2026-04-17 15:13:42 +0900

    sudo

--- common/scripts/14.jupyter/1.setup_directory.sh
+++ common/scripts/14.jupyter/1.setup_directory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 各GPUノードでディレクトリ作成
 for node in dlcsv1 dlcsv2; do
-    ssh $node "mkdir -p /mnt/jupyter-workspace /mnt/datasets && chmod 755 /mnt/jupyter-workspace /mnt/datasets"
+    ssh $node "sudo mkdir -p /mnt/jupyter-workspace /mnt/datasets && sudo chmod 755 /mnt/jupyter-workspace /mnt/datasets"
     echo "Directories created on $node"
 done
